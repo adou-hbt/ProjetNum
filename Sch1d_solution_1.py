4b4d887ec35fbc81733bd5b07b1e5fa8a5fcf012
Tris <166401431+Triiiss@users.noreply.github.com>
2025-06-06 16:29:23 +0200

    Add files via upload

--- Sch1d_solution_1.py
+++ Sch1d_solution_1.py
@@ -5,17 +5,8 @@ import matplotlib.animation as animation
 import time
 
 start_time = time.time()
-
-def init():
-    line.set_data([], [])
-    return line,
-
-def animate(j):
-    line.set_data(o, final_densite[j,:]) #Crée un graphique pour chaque densite sauvegarde
-    return line,
-
 #~CALCUL ETATS STATIONNAIRES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def Etats_stationnaires(n_Etats,m,L):
+def Energie(n_Etats,m,L):
     hbar = 1.055e-34       # constante de Planck réduite
     eV = 1.602e-19
     
@@ -23,7 +14,7 @@ def Etats_stationnaires(n_Etats,m,L):
     for n in range(1 ,n_Etats+1):
         En = pow((hbar * n * np.pi),2) / (2 * m * pow(L*pow(10,-9),2))
         En_eV = En/eV
-        Etats[n-1] = En_eV - v0
+        Etats[n-1] = En_eV + v0
     return Etats
 
 def Vitesse(Etats,m):
@@ -53,10 +44,11 @@ sigma=0.05
 A=1/(math.sqrt(sigma*math.sqrt(math.pi)))
 
 #Variables de l'équation de schrodinger
-v0=-20
+v0=-4000
 e=5#Valeur du rapport E/V0
 E=e*v0
-k=math.sqrt(2*abs(E))   #nombre d'éléctrons
+m = 9.109e-31          # masse d’un éléctron
+L = 0.6
 
 # Initialisation des tableaux
 o = np.linspace(0, (nx - 1) * dx, nx)
@@ -64,65 +56,80 @@ V = np.zeros(nx)
 #V[o >= 1] = v0  # Potentiel
 V[(o >= 0.8) & (o<=0.9)] = v0  # Potentiel
 
-#Equation Schrödinger
-cpt = A * np.exp(1j * k * o - ((o - xc) ** 2) / (2 * (sigma ** 2)))
-re=np.zeros(nx)
-re[:]=np.real(cpt[:])
-b=np.zeros(nx)
-im=np.zeros(nx)
-im[:]=np.imag(cpt[:])
-
-#Densité
-densite=np.zeros((nt,nx))
-densite[0,:] = np.absolute(cpt[:]) ** 2
-final_densite=np.zeros((n_frame,nx))
-
-
-m = 9.109e-31          # masse d’un éléctron
-L = 0.45
-
-Etats = Etats_stationnaires(10,m,L)
+Etats = Energie(2,m,L)
 vitesse = Vitesse(Etats,m)
-print(Etats)
-print(vitesse)
-#~MODELISATION FONCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-it=0
-for i in range(1, nt):
-    if i % 2 != 0:
-        b[1:-1]=im[1:-1]
-        im[1:-1] = im[1:-1] + s * (re[2:] + re[:-2]) - 2 * re[1:-1] * (s + V[1:-1] * dt)
-        densite[i,1:-1] = re[1:-1]*re[1:-1] + im[1:-1]*b[1:-1]
-    else:
-        re[1:-1] = re[1:-1] - s * (im[2:] + im[:-2]) + 2 * im[1:-1] * (s + V[1:-1] * dt)
-
-for i in range(1,nt):
-    if((i-1)%1000==0):
-        it+=1
-        final_densite[it][:]=densite[i][:]
 
 #~ANIMATION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 plot_title = "Marche Ascendante avec E/Vo="+str(e)
 fig = plt.figure() # initialise la figure principale
-line, = plt.plot([], [])
-plt.ylim(0,13)
+colors = ['red', 'green', 'blue', 'orange', 'purple']
+lines = [plt.plot([], [], label=f"État {i+1}", color=colors[i % len(colors)])[0] for i in range(len(Etats))]
+plt.ylim(-10,13)
 plt.xlim(0,2)
 
 plt.plot(o,V,label="Potentiel")
 
-for i in Etats:
-    plt.plot([0,2],[i,i], color="red")
+for i in range(len(Etats)):
+    plt.plot([0,2],[Etats[i],Etats[i]], color=colors[i])
 
 plt.title(plot_title)
 plt.xlabel("x")
 plt.ylabel("Densité de probabilité de présence")
 plt.legend() #Permet de faire apparaitre la legende
 
-ani = animation.FuncAnimation(fig,animate,init_func=init, frames=nd, blit=False, interval=100, repeat=False)
-#file_name = 'paquet_onde_e='+str(e)+'.mp4'
-#ani.save(file_name, writer = animation.FFMpegWriter(fps=120, bitrate=5000))
+ani = []
+all_final_densites = []
+
+for j in Etats:   
+    k=math.sqrt(2*abs(j))   #nombre d'éléctrons
+    #Equation Schrödinger
+    cpt = A * np.exp(1j * k * o - ((o - xc) ** 2) / (2 * (sigma ** 2)))
+    re=np.zeros(nx)
+    re[:]=np.real(cpt[:])
+    b=np.zeros(nx)
+    im=np.zeros(nx)
+    im[:]=np.imag(cpt[:])
+
+    #Densité
+    densite=np.zeros((nt,nx))
+    densite[0,:] = np.absolute(cpt[:]) ** 2
+    final_densite=np.zeros((n_frame,nx))
+    all_final_densites.append(final_densite)
+
+    #~MODELISATION FONCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    it=0
+    for i in range(1, nt):
+        if i % 2 != 0:
+            b[1:-1]=im[1:-1]
+            im[1:-1] = im[1:-1] + s * (re[2:] + re[:-2]) - 2 * re[1:-1] * (s + V[1:-1] * dt)
+            densite[i,1:-1] = re[1:-1]*re[1:-1] + im[1:-1]*b[1:-1]
+        else:
+            re[1:-1] = re[1:-1] - s * (im[2:] + im[:-2]) + 2 * im[1:-1] * (s + V[1:-1] * dt)
+
+    for i in range(1,nt):
+        if((i-1)%1000==0):
+            it+=1
+            final_densite[it][:]=densite[i][:]
+
+
+def init():
+    for line in lines:
+        line.set_data([], [])
+    return lines
+
+def animate(j):
+    for i, line in enumerate(lines):
+        line.set_data(o, all_final_densites[i][j, :])
+    return lines
+
+ani.append(animation.FuncAnimation(fig,animate,init_func=init, frames=nd, blit=False, interval=100, repeat=False))
+plt.legend()
+plt.grid(True)
 plt.show()
 
 
+#file_name = 'paquet_onde_e='+str(e)+'.mp4'
+#ani.save(file_name, writer = animation.FFMpegWriter(fps=120, bitrate=5000))
 # end_time = time.time()
 # elapsed_time = end_time - start_time
 # print(f"Elapsed Time: {elapsed_time} seconds")
